1a83188aa8330d9a1e4c9f1f806be49c8d1022a8
guilhermebene <gui.martinsbn@hotmail.com>
2017-09-23 22:57:12 -0300

    Added functions to correct folder

--- Dados Institutos/nomesResponsaveis.py
+++ Dados Institutos/nomesResponsaveis.py
@@ -4,14 +4,17 @@ import functions as fc
 numeroInstituto = input("Insira o número do instituto\n")
 pastaInstituto = fc.procurarNomes(int(numeroInstituto))
 responsavel = ""
+
 files = [f for f in os.listdir(pastaInstituto)]
+logResponsaveis = open("logResponsaveis.txt", "a+")
+if pastaInstituto not in logResponsaveis.readlines():
+ logResponsaveis.write(pastaInstituto + "\n")
+
 for filename in files:
- dados = open(pastaInstituto + "/" + filename, "r", encoding = "utf8")
- for line in dados:
-  x = dados.readline()
-  print(x)
-  if(x == "Responsável:"):
-   responsavel = dados.readline()
-   break
+ name = pastaInstituto + "/" + filename
+ dados = open(name, "r", encoding = "utf8")
+ responsavel = fc.procurarResponsavel(dados)
+ if responsavel not in logResponsaveis.readlines():
+  logResponsaveis.write(responsavel + "\n")
  dados.close()
- print(responsavel)
+logResponsaveis.close()
