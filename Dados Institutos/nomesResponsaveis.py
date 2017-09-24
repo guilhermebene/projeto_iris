56876175d01169b8e691d1f5d261c92573aad441
guilhermebene <gui.martinsbn@hotmail.com>
2017-09-23 21:01:11 -0300

    Update nomesResponsaveis

--- Dados Institutos/nomesResponsaveis.py
+++ Dados Institutos/nomesResponsaveis.py
@@ -3,14 +3,15 @@ import functions as fc
 
 numeroInstituto = input("Insira o número do instituto\n")
 pastaInstituto = fc.procurarNomes(int(numeroInstituto))
-
-files = [f for f in os.listdir("pastaInstituto")]
+responsavel = ""
+files = [f for f in os.listdir(pastaInstituto)]
 for filename in files:
  dados = open(pastaInstituto + "/" + filename, "r", encoding = "utf8")
  for line in dados:
   x = dados.readline()
+  print(x)
   if(x == "Responsável:"):
-   responsavel = line
+   responsavel = dados.readline()
    break
  dados.close()
-print(responsavel)
+ print(responsavel)
